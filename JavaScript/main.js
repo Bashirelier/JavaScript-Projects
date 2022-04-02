39f7d9b3586eda12bb46695cf42e5c0d59325fb0
Bashir Elier <97809784+Bashirelier@users.noreply.github.com>
2022-04-02 15:36:04 +0100

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,13 @@
+
+var Y = "Press OK if you want to get stated"
+window.alert(Y) 
+
+var A = 10; //assign variable
+var B = 30; //assign variable 
+document.write("What is 10+30?"); 
+
+function My_First_Function() { //defining a function and naming it
+    var str = "40"; //defining a variable and giving it a string value
+    var result = str.fontcolor("green"); //using the fontcolor method on str varible
+    document.getElementById("green_text").innerHTML = result; //Putting the value of results into HTML elemnt with greentext id
+}
