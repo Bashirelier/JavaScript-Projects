ad83f4f60dcbb12886de026dab8d3156b8430e8e
Bashir Elier <97809784+Bashirelier@users.noreply.github.com>
2022-04-03 03:44:48 +0100

    Add files via upload

---
+++ JavaScript/Project2_function/JS/main.js
@@ -0,0 +1,13 @@
+ 
+ function My_Second_Function() {  
+     
+    document.getElementById("test").style.fontSize = "250px"; 
+    document.getElementById("test").style.color = "red";
+    document.getElementById("test").style.backgroundColor = "yellow";   
+
+    var sentence = "It";
+    sentence += " Works";
+    document.getElementById("test").innerHTML = sentence;
+}
+
+
