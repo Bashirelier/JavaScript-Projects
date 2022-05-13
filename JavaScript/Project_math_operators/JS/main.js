6f188aaa42ea1bb5db53b2ffc05ed0cfef0a9c45
Bashir Elier <97809784+Bashirelier@users.noreply.github.com>
2022-05-13 06:02:04 +0100

    Add files via upload

---
+++ JavaScript/Project_math_operators/JS/main.js
@@ -0,0 +1,31 @@
+function addition_function () {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 =" + addition;
+}
+
+function subtraction_function () {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 =" + subtraction;
+}
+function multiplication () {
+    var simple_math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 * 8 =" + simple_math;
+}
+function division () {
+    var simple_math = 36 / 6;
+    document.getElementById("Math").innerHTML = "36 / 6 =" + simple_math;
+}
+
+function more_math () {
+    var simple_math = (1 + 2) * 10 / 2-5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied b 10, divided in half and then subtracted by 5 equals" + simple_math;
+}
+function modulus_operator () {
+    var simple_math = 36 % 6;
+    document.getElementById("Math").innerHTML = "when you divide 36 by 6 you have a remainder of:" + simple_math;
+}
+
+function ngation_operator () {
+    var x = 2;
+    document.getElementById("Math").innerHTML =-x;
+}
